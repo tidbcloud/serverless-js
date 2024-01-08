0233fbd1e2c358be27cb436cd62daae80b142a2c
shiyuhang <1136742008@qq.com>
2024-01-08 17:22:16 +0800

    alter id

--- src/serverless.ts
+++ src/serverless.ts
@@ -61,5 +61,15 @@ export async function postQuery<T>(config: Config, body, session = '', isolation
 
 function generateUniqueId() {
   const datetime = new Date().toISOString().replace(/[^\d]/g, '').slice(0, 14);
-  return `${datetime}${Math.random().toString(36).substring(2, 12)}${Math.random().toString(36).substring(2, 12)}`
+  return `${datetime}${randomString(20)}`
+}
+
+function randomString(n) {
+  let result = '';
+  const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  const l = characters.length;
+  for (let i = 0; i < n; i++) {
+    result += characters[Math.floor(Math.random() * l)]
+  }
+  return result;
 }
